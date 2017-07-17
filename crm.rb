9a4cb55ba15add779b09ed6e95f19ec2537135be
nadiaivannova <nadiaivannova@gmail.com>
2017-07-17 16:10:16 -0400

    added post and redirected input form now works

--- crm.rb
+++ crm.rb
@@ -19,6 +19,13 @@ get '/contacts/new' do
   erb :new
 end
 
+post '/contacts' do
+
+@contact = Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], note: params[:note])
+
+redirect('/contacts/new')
+end
+
 get '/contacts/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
   if @contact
@@ -29,10 +36,12 @@ get '/contacts/:id' do
   erb :show_contact
 end
 
+
 get '/about' do
 
   erb :about
 end
+
 after do
   ActiveRecord::Base.connection.close
 end
